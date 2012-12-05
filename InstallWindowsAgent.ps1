82d189d64355d044f1a3e40ffda823aafd605190
Ameer Deen <ameer.deen@servicemesh.com>
2012-12-05 20:59:09 +1100

    Updated windows agent installer with correct urls

---
+++ InstallWindowsAgent.ps1
@@ -0,0 +1,19 @@
+﻿
+# Create OSSecHub Home Folder
+$folderName = "c:\ossechub\"
+mkdir -force  $folderName
+cd $folderName
+
+# Download Helper Library
+$downloadUrl1 = "https://raw.github.com/writeameer/test/master/OSSecHub.WindowsAgent.dll"
+$downloadFile = Join-Path $folderName $downloadUrl1.Split('/')[-1]
+(New-Object System.Net.WebClient).DownloadFile($downloadUrl1,$downloadFile)
+
+# Download Helper Library
+$downloadUrl2 = "https://raw.github.com/writeameer/test/master/Installer.ps1"
+$downloadFile = Join-Path $folderName $downloadUrl2.Split('/')[-1]
+(New-Object System.Net.WebClient).DownloadFile($downloadUrl2,$downloadFile)
+
+# Run Installer
+
+& "$downloadFile"
